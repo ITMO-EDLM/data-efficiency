8673a6830f54fdd62f41ba953cac73af2f8adab5
favead <diablo3_monk@mail.ru>
2025-10-18 18:49:06 +0300

    Added imports on module level

--- src/data_efficiency/utils/__init__.py
+++ src/data_efficiency/utils/__init__.py
@@ -1,3 +1,6 @@
 from data_efficiency.utils.data import build_dataloader, upload_dataset
+from data_efficiency.utils.loss import get_loss
+from data_efficiency.utils.metrics import accuracy, f1
+from data_efficiency.utils.tracker import MetricTracker
 
-__all__ = ["build_dataloader", "upload_dataset"]
+__all__ = ["accuracy", "build_dataloader", "f1", "get_loss", "MetricTracker", "upload_dataset"]
