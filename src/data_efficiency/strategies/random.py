67beafbe2eeab9c6a88448e53be61e75bfb691cc
favead <diablo3_monk@mail.ru>
2025-11-22 10:57:02 +0300

    Fix kwargs

--- src/data_efficiency/strategies/random.py
+++ src/data_efficiency/strategies/random.py
@@ -5,7 +5,7 @@ from data_efficiency.strategies.base import DataSelectionStrategy
 
 
 class RandomDatasetSelectionStrategy(DataSelectionStrategy):
-    def select(self, dataset: TokenizedDataset, limit: int):
+    def select(self, dataset: TokenizedDataset, limit: int, **strategy_kwargs):
         """
         Return random dataset limit size.
 
